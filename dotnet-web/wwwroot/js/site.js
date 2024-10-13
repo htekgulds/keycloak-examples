4beb833c03badd743b9cd36aed449085ac60996c
Hasan TEKGÜL <htekgulds@gmail.com>
2024-10-13 21:19:39 +0300

    client deneme

--- dotnet-web/wwwroot/js/site.js
+++ dotnet-web/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(() => {
+  $.get('/users/me', data => {
+    console.log('Data', data)
+    $('#user').text(JSON.stringify(data, null, 2))
+  })
+})
